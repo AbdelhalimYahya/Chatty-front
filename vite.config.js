9934defdbae48fd87660d67abfecd8346963e575
abdelhalim <abdelhalim@gmail.com>
2025-08-10 12:00:34 +0300

    vite edit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: '../frontend/dist'  // Output folder
+    outDir: 'dist'  // Output folder
   }
 })
